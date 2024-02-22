f2eaf23236a6f2a1e95d52f201fe5b192163b85c
Lee <ultralmantigar@gmail.com>
2024-02-22 17:45:48 +0800

    11

---
+++ test1.c
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: test1.c
+    > Author: Kir
+    > Mail: ultralmantigar@gmail.com
+    > Created Time: 2/22/2024 5:44:51 PM
+ ************************************************************************/
+
+#include<stdio.h>
+
+this is a test string.
